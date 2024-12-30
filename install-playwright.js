0f3c2fe1f64b1a477bcedc040022c3eeece8a170
Gurumita <g.gurumita@gmail.com>
2024-12-30 18:17:00 +0530

    Normalize line endings

---
+++ install-playwright.js
@@ -0,0 +1,7 @@
+// install-playwright.js
+const { execSync } = require('child_process');
+
+console.log('Installing Playwright browsers...');
+execSync('npx playwright install', { stdio: 'inherit' });
+
+console.log('Playwright browsers installed successfully!');
